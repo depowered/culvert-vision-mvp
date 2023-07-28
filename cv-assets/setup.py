7eff779df61f2000841a8ed63334fecf185c5623
Devin Power <dpwr16@gmail.com>
2023-07-28 14:10:14 -0500

    scaffold dagster project

---
+++ cv-assets/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="cv_assets",
+    packages=find_packages(exclude=["cv_assets_tests"]),
+    install_requires=[
+        "dagster",
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
