0973239758317020bad1de1fffd0f7eb11a95309
Devin Power <dpwr16@gmail.com>
2023-07-28 23:45:48 -0500

    refactor bash script to string template

--- cv-assets/cv_assets/vectors/usgs_wesm/__init__.py
+++ cv-assets/cv_assets/vectors/usgs_wesm/__init__.py
@@ -1,7 +1,8 @@
 import subprocess
 from pathlib import Path
+from string import Template
 
-from dagster import asset, file_relative_path
+from dagster import asset
 
 from cv_assets import config
 
@@ -9,26 +10,43 @@ from cv_assets import config
 @asset
 def usgs_wesm_gpkg() -> Path:
     """Download USGS Workunit Extent Spatial Metadata (WESM) GeoPackage from source"""
-    url = "https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/metadata/WESM.gpkg"
-    output = config.CV_ASSETS_DATA / "raw/WESM.gpkg"
-    cmd = f"curl --create-dirs --output {output} {url}"
-    subprocess.run(cmd.split())
-    return output
+
+    cmd = Template("curl --create-dirs --output $output $url")
+
+    subprocess.run(
+        args=cmd.substitute(
+            output=config.CV_ASSETS_DATA / "raw/WESM.gpkg",
+            url="https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/metadata/WESM.gpkg",
+        ),
+        shell=True,
+        check=True,
+    )
+
+    return config.CV_ASSETS_DATA / "raw/WESM.gpkg"
 
 
 @asset
 def usgs_wesm_parquet(usgs_wesm_gpkg: Path) -> Path:
-    """Filter and reproject USGS WESM GeoPackage to GeoParquet"""
+    """Filter and reproject USGS WESM GeoPackage to Parquet"""
 
-    input = usgs_wesm_gpkg
-    output = config.CV_ASSETS_DATA / "interim/usgs_wesm.parquet"
-    to_srs = f"EPSG:{config.TO_EPSG}"
-    select_statement = "SELECT * FROM WESM WHERE workunit LIKE 'MN%'"
-
-    output.parent.mkdir(parents=True, exist_ok=True)
-    script = Path(file_relative_path(__file__, "./raw_to_parquet.sh")).resolve()
-    subprocess.run(
-        ["bash", f"{script}", f"{output}", f"{input}", to_srs, select_statement]
+    cmd = Template(
+        """
+        ogr2ogr \
+            -f Parquet \
+            -t_srs $to_srs \
+            -sql "SELECT * FROM WESM WHERE workunit LIKE 'MN%'" \
+            $output $input
+        """
     )
 
-    return output
+    subprocess.run(
+        args=cmd.substitute(
+            input=usgs_wesm_gpkg,
+            output=config.CV_ASSETS_DATA / "interim/usgs_wesm.parquet",
+            to_srs=f"EPSG:{config.TO_EPSG}",
+        ),
+        shell=True,
+        check=True,
+    )
+
+    return config.CV_ASSETS_DATA / "interim/usgs_wesm.parquet"
