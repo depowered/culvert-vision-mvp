42111d88e226d937c9456bad2e8578bb6f2b513f
Devin Power <dpwr16@gmail.com>
2023-07-30 13:58:19 -0500

    create mndnr_culvert_inventory assets

--- cv-assets/cv_assets/__init__.py
+++ cv-assets/cv_assets/__init__.py
@@ -1,9 +1,9 @@
 from dagster import Definitions, load_assets_from_modules
 
-from cv_assets.vectors import usgs_opr_tesm, usgs_wesm
+from cv_assets.vectors import mndnr_culvert_inventory, usgs_opr_tesm, usgs_wesm
 
 vector_assets = load_assets_from_modules(
-    [usgs_wesm, usgs_opr_tesm], group_name="vectors"
+    [mndnr_culvert_inventory, usgs_opr_tesm, usgs_wesm], group_name="vectors"
 )
 
 defs = Definitions(assets=vector_assets)
---
+++ cv-assets/cv_assets/vectors/mndnr_culvert_inventory.py
@@ -0,0 +1,122 @@
+import subprocess
+from string import Template
+
+from dagster import asset
+
+from cv_assets.config import get_settings
+from cv_assets.file_asset import VectorFileAsset
+
+settings = get_settings()
+TARGET_EPSG = settings.target_epsg
+
+
+@asset
+def raw_mndnr_culvert_inventory() -> VectorFileAsset:
+    """Download MnDNR Culvert Inventory Suite as an ESRI File Geodatabase.
+
+    The File Geodatabase source is chosen because the Shapefile and GeoPackage
+    sources have truncated column names."""
+
+    output = VectorFileAsset("raw_mndnr_culvert_inventory.gdb.zip")
+
+    cmd = Template("curl --create-dirs --output $output $url")
+
+    subprocess.run(
+        args=cmd.substitute(
+            output=output.get_path(),
+            url="https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_dnr/struc_culvert_inventory_pub/fgdb_struc_culvert_inventory_pub.zip",
+        ),
+        shell=True,  # Allows args to be passed as a string
+        check=True,  # Prevents cmd from failing silently
+    )
+
+    return output
+
+
+@asset
+def stg_mndnr_stream_crossing_summary(
+    raw_mndnr_culvert_inventory: VectorFileAsset,
+) -> VectorFileAsset:
+    """Extract and reproject the Stream_Crossing_Summary layer from the MnDOT Culvert
+    Inventory and write to Parquet"""
+
+    output = VectorFileAsset("stg_mndnr_stream_crossing_summary.parquet")
+
+    cmd = Template(
+        """
+        ogr2ogr \
+            -f Parquet \
+            -t_srs $to_srs \
+            -sql "SELECT * FROM Stream_Crossing_Summary" \
+            $output $input
+        """
+    )
+
+    subprocess.run(
+        args=cmd.substitute(
+            to_srs=f"EPSG:{TARGET_EPSG}",
+            output=output.get_path(),
+            input=raw_mndnr_culvert_inventory.get_path(),
+        ),
+        shell=True,  # Allows args to be passed as a string
+        check=True,  # Prevents cmd from failing silently
+    )
+
+    return output
+
+
+@asset
+def int_mndnr_culvert_opening(
+    raw_mndnr_culvert_inventory: VectorFileAsset,
+) -> VectorFileAsset:
+    """Extract and reproject the Culvert_Opening layer from the MnDOT Culvert
+    Inventory to an intermediate GeoPackage.
+
+    The source layer is incompatible with the Parquet driver, so the GeoPackage
+    is used as a compatibility intermediate."""
+
+    output = VectorFileAsset("int_mndnr_culvert_opening.gpkg")
+
+    cmd = Template(
+        """
+        ogr2ogr \
+            -f GPKG \
+            -t_srs $to_srs \
+            -sql "SELECT * FROM Culvert_Opening" \
+            $output $input
+        """
+    )
+
+    subprocess.run(
+        args=cmd.substitute(
+            to_srs=f"EPSG:{TARGET_EPSG}",
+            output=output.get_path(),
+            input=raw_mndnr_culvert_inventory.get_path(),
+        ),
+        shell=True,  # Allows args to be passed as a string
+        check=True,  # Prevents cmd from failing silently
+    )
+
+    return output
+
+
+@asset
+def stg_mndnr_culvert_opening(
+    int_mndnr_culvert_opening: VectorFileAsset,
+) -> VectorFileAsset:
+    """Convert Culvert_Opening intermediate GeoPackage to Parquet"""
+
+    output = VectorFileAsset("stg_mndnr_culvert_opening.parquet")
+
+    cmd = Template("ogr2ogr -f Parquet $output $input")
+
+    subprocess.run(
+        args=cmd.substitute(
+            output=output.get_path(),
+            input=int_mndnr_culvert_opening.get_path(),
+        ),
+        shell=True,  # Allows args to be passed as a string
+        check=True,  # Prevents cmd from failing silently
+    )
+
+    return output
