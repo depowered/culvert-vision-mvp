b984871c0ccc26006d26a7bf23f0a3fd6d91b0aa
Devin Power <dpwr16@gmail.com>
2023-07-29 23:45:32 -0500

    create FileAsset and VectorFileAsset

---
+++ cv-assets/cv_assets/file_asset.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from cv_assets.settings import Settings
+
+
+FILE_ASSET_STORAGE_DIR = Settings().file_asset_storage_dir.resolve()
+
+
+class FileAsset:
+    _base_path: Path = FILE_ASSET_STORAGE_DIR
+
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def _make_parent_directories(self) -> None:
+        self._base_path.mkdir(parents=True, exist_ok=True)
+
+    def get_path(self) -> Path:
+        self._make_parent_directories()
+        return self._base_path / self.filename
+
+
+class VectorFileAsset(FileAsset):
+    _base_path: Path = FILE_ASSET_STORAGE_DIR / "vector"
--- cv-assets/cv_assets/vectors/usgs_wesm.py
+++ cv-assets/cv_assets/vectors/usgs_wesm.py
@@ -1,52 +1,63 @@
 import subprocess
-from pathlib import Path
 from string import Template
 
 from dagster import asset
 
-from cv_assets import config
+from cv_assets.file_asset import VectorFileAsset
+from cv_assets.settings import Settings
+
+
+TARGET_EPSG = Settings().target_epsg
 
 
 @asset
-def usgs_wesm_gpkg() -> Path:
+def raw_usgs_wesm() -> VectorFileAsset:
     """Download USGS Workunit Extent Spatial Metadata (WESM) GeoPackage from source"""
+
+    output = VectorFileAsset("raw_usgs_wesm.gpkg")
+
+    # The file is large, avoid redownloading if it already exists
+    if output.get_path().exists:
+        return output
 
     cmd = Template("curl --create-dirs --output $output $url")
 
     subprocess.run(
         args=cmd.substitute(
-            output=config.CV_ASSETS_DATA / "raw/WESM.gpkg",
+            output=output.get_path(),
             url="https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/metadata/WESM.gpkg",
         ),
         shell=True,
         check=True,
     )
 
-    return config.CV_ASSETS_DATA / "raw/WESM.gpkg"
+    return output
 
 
 @asset
-def usgs_wesm_parquet(usgs_wesm_gpkg: Path) -> Path:
+def stg_usgs_wesm(raw_usgs_wesm: VectorFileAsset) -> VectorFileAsset:
     """Filter and reproject USGS WESM GeoPackage to Parquet"""
+
+    output = VectorFileAsset("stg_usgs_wesm.parquet")
 
     cmd = Template(
         """
         ogr2ogr \
             -f Parquet \
             -t_srs $to_srs \
-            -sql "SELECT * FROM WESM WHERE workunit LIKE 'MN%'" \
+            -sql "SELECT * FROM WESM WHERE workunit LIKE 'MN%' AND ql IN ('QL 0', 'QL 1')" \
             $output $input
         """
     )
 
     subprocess.run(
         args=cmd.substitute(
-            input=usgs_wesm_gpkg,
-            output=config.CV_ASSETS_DATA / "interim/usgs_wesm.parquet",
-            to_srs=f"EPSG:{config.TO_EPSG}",
+            output=output.get_path(),
+            input=raw_usgs_wesm.get_path(),
+            to_srs=f"EPSG:{TARGET_EPSG}",
         ),
         shell=True,
         check=True,
     )
 
-    return config.CV_ASSETS_DATA / "interim/usgs_wesm.parquet"
+    return output
