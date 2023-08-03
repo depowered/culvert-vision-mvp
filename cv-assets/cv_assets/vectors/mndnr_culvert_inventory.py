07c1c4916da24df31699da8d235465f772dc7708
Devin Power <dpwr16@gmail.com>
2023-08-03 15:59:50 -0500

    create pg_stg_mndnr assets

--- cv-assets/cv_assets/vectors/mndnr_culvert_inventory.py
+++ cv-assets/cv_assets/vectors/mndnr_culvert_inventory.py
@@ -4,7 +4,9 @@ from string import Template
 from dagster import asset
 
 from cv_assets.config import get_settings
+from cv_assets.resources.postgis import PGTable, PostGISResource
 from cv_assets.resources.vector_file_asset import VectorFileAsset
+from cv_assets.vectors.load_pg_table import load_table_from_parquet
 
 settings = get_settings()
 TARGET_EPSG = settings.target_epsg
@@ -66,6 +68,23 @@ def stg_mndnr_stream_crossing_summary(
 
 
 @asset
+def pg_stg_mndnr_stream_crossing_summary(
+    stg_mndnr_stream_crossing_summary: VectorFileAsset, postgis: PostGISResource
+) -> PGTable:
+    """Load Stream Crossing Summary Parquet into PostGIS table"""
+    output = PGTable(schema="mn", table="mndnr_stream_crossing")
+
+    load_table_from_parquet(
+        input=stg_mndnr_stream_crossing_summary.get_path(),
+        dsn=postgis.dsn,
+        schema=output.schema,
+        table=output.table,
+    )
+
+    return output
+
+
+@asset
 def int_mndnr_culvert_opening(
     raw_mndnr_culvert_inventory: VectorFileAsset,
 ) -> VectorFileAsset:
@@ -117,6 +136,23 @@ def stg_mndnr_culvert_opening(
         ),
         shell=True,  # Allows args to be passed as a string
         check=True,  # Prevents cmd from failing silently
+    )
+
+    return output
+
+
+@asset
+def pg_stg_mndnr_culvert_opening(
+    stg_mndnr_culvert_opening: VectorFileAsset, postgis: PostGISResource
+) -> PGTable:
+    """Load Stream Crossing Summary Parquet into PostGIS table"""
+    output = PGTable(schema="mn", table="mndnr_culvert_opening")
+
+    load_table_from_parquet(
+        input=stg_mndnr_culvert_opening.get_path(),
+        dsn=postgis.dsn,
+        schema=output.schema,
+        table=output.table,
     )
 
     return output
