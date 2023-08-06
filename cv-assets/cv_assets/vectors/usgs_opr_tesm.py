e890dd54cf0269bf3771ff626b41b0bc0f083dfd
Devin Power <dpwr16@gmail.com>
2023-08-06 12:38:30 -0500

    refactor usgs_opr_tesm assets to use LocalVectorFileStorage

--- cv-assets/cv_assets/vectors/usgs_opr_tesm.py
+++ cv-assets/cv_assets/vectors/usgs_opr_tesm.py
@@ -3,8 +3,8 @@ from string import Template
 from dagster import asset
 
 from cv_assets.config import get_settings
+from cv_assets.resources.file_storage.vector import LocalVectorFileStorage, VectorFile
 from cv_assets.resources.postgis import PGTable, PostGISResource
-from cv_assets.resources.vector_file_asset import VectorFileAsset
 from cv_assets.utils import run_shell_cmd
 from cv_assets.vectors.load_pg_table import load_table_from_parquet
 from cv_assets.vectors.usgs_wesm import workunit_ids  # noqa F411
@@ -14,21 +14,21 @@ TARGET_EPSG = settings.target_epsg
 
 
 @asset
-def raw_usgs_opr_tesm() -> VectorFileAsset:
+def raw_usgs_opr_tesm(vector_storage: LocalVectorFileStorage) -> VectorFile:
     """Download USGS Original Product Resolution (OPR)
     Tile Extent Spatial Metadata (TESM) GeoPackage from source"""
 
-    output = VectorFileAsset("raw_usgs_opr_tesm.gpkg")
+    output = vector_storage.get_file_by_filename("raw_usgs_opr_tesm.gpkg")
 
     # The file is large, avoid redownloading if it already exists
-    if output.get_path().exists():
+    if output.path.exists():
         return output
 
     cmd = Template("curl --create-dirs --output $output $url")
 
     run_shell_cmd(
         cmd=cmd,
-        output=output.get_path(),
+        output=output.path,
         url="https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/OPR/FullExtentSpatialMetadata/OPR_TESM.gpkg",
     )
 
@@ -37,11 +37,13 @@ def raw_usgs_opr_tesm() -> VectorFileAsset:
 
 @asset
 def stg_usgs_opr_tesm(
-    raw_usgs_opr_tesm: VectorFileAsset, workunit_ids: list[int]  # noqa F811
-) -> VectorFileAsset:
+    vector_storage: LocalVectorFileStorage,
+    raw_usgs_opr_tesm: VectorFile,
+    workunit_ids: list[int],  # noqa F811
+) -> VectorFile:
     """Filter and reproject USGS OPR TESM GeoPackage to Parquet"""
 
-    output = VectorFileAsset("stg_usgs_opr_tesm.parquet")
+    output = vector_storage.get_file_by_filename("stg_usgs_opr_tesm.parquet")
 
     cmd = Template(
         """
@@ -57,8 +59,8 @@ def stg_usgs_opr_tesm(
         cmd=cmd,
         to_srs=f"EPSG:{TARGET_EPSG}",
         workunit_ids=tuple(workunit_ids),
-        output=output.get_path(),
-        input=raw_usgs_opr_tesm.get_path(),
+        output=output.path,
+        input=raw_usgs_opr_tesm.path,
     )
 
     return output
@@ -66,13 +68,13 @@ def stg_usgs_opr_tesm(
 
 @asset
 def pg_stg_usgs_opr_tesm(
-    stg_usgs_opr_tesm: VectorFileAsset, postgis: PostGISResource
+    stg_usgs_opr_tesm: VectorFile, postgis: PostGISResource
 ) -> PGTable:
     """Load USGS OPR TESM Parquet into PostGIS table"""
     output = PGTable(schema="mn", table="usgs_opr_tiles")
 
     load_table_from_parquet(
-        input=stg_usgs_opr_tesm.get_path(),
+        input=stg_usgs_opr_tesm.path,
         dsn=postgis.dsn,
         schema=output.schema,
         table=output.table,
